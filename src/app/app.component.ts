814b43418e7088708d81cee786e424985dbaa221
Linh <huyenlinhvu.pro@gmail.com>
2022-12-16 16:25:14 +0100

    observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularhttp';
+export class AppComponent implements OnInit {
+  title = 'Observable';
+
+  myObservable = new Observable((observer) => {
+    console.log('Observable starts');
+    setTimeout(() => { observer.next('1')}, 1000)
+    setTimeout(() => { observer.next('2')}, 2000)
+    setTimeout(() => { observer.next('3')}, 3000)
+    setTimeout(() => { observer.next('4')}, 4000)
+    setTimeout(() => { observer.next('5')}, 5000)
+  });
+
+  ngOnInit() {
+    this.myObservable.subscribe((val) => {
+      console.log(val);
+    });
+  }
 }
